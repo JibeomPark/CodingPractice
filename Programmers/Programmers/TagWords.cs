0a580a2ada1fe914b40031af722c60f4e4a3f63c
parkjibeom <saver0421@bluepotion.co.kr>
2023-10-19 12:25:28 +0900

    [Level 1] 영어 끝말잇기

---
+++ Programmers/Programmers/TagWords.cs
@@ -0,0 +1,48 @@
+﻿namespace Programmers
+{
+    public class TagWords
+    {
+        public int[] solution(int n, string[] words)
+        {
+            int currentRound = 1;
+            int currentPlayer = 1;
+            char prevLastChar = words.First().First();
+            Dictionary<char, List<string>> m_answerDic = new Dictionary<char, List<string>>();
+
+            foreach (string word in words)
+            {
+                if (word.First() != prevLastChar)
+                    return new int[] { currentPlayer, currentRound };
+
+                prevLastChar = word[word.Length - 1];
+
+                if (m_answerDic.TryGetValue(word.First(), out var list))
+                {
+                    if (list.Contains(word))
+                    {
+                        return new int[] { currentPlayer, currentRound };
+                    }
+                    else
+                    {
+                        list.Add(word);
+                    }
+                }
+                else
+                {
+                    var newList = new List<string>() { word };
+                    m_answerDic.Add(word.First(), newList);
+                }
+                
+                currentPlayer++;
+
+                if (currentPlayer > n)
+                {
+                    currentPlayer = 1;
+                    currentRound++;
+                }
+            }
+
+            return new int[] { 0, 0 };
+        }
+    }
+}
--- Programmers/Programmers/main.cs
+++ Programmers/Programmers/main.cs
@@ -1,13 +1,17 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
 namespace Programmers
 {
     public class main
     {
         public static void Main(string[] args)
         {
-            var m1 = new SelfPlay();
-            int[] a = new int[] { 2, 3,4,5,6,7,8,9,1 };
-            Console.WriteLine(m1.solution(a));
+            var m1 = new TagWords();
+            var arr = new string[] {"hello", "o", "even", "never", "now", "world", "draw"};
+            Console.WriteLine(m1.solution(2, arr)[0].ToString() +  m1.solution(2, arr)[1].ToString());
         }
     }
 }
