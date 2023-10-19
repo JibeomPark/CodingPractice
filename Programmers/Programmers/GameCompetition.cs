28bc10db29d1d01b3b3f1d0abdd8c14e06f80d6b
parkjibeom <saver0421@bluepotion.co.kr>
2023-10-19 15:00:59 +0900

    [Level 1] 게임 대회

---
+++ Programmers/Programmers/GameCompetition.cs
@@ -0,0 +1,33 @@
+﻿namespace Programmers
+{
+    public class GameCompetition
+    {
+        public int solution(int n, int a, int b)
+        {
+            int answer = 1;
+
+            while (IsTheyMeet(a, b) == false)
+            {
+                a = NextRount(a);
+                b = NextRount(b);
+                answer++;
+            }
+
+            return answer;
+        }
+
+        public bool IsTheyMeet(int a, int b)
+        {
+            return (a % 2 == 0 && a - 1 == b ||
+               b % 2 == 0 && b - 1 == a);
+        }
+
+        public int NextRount(int num)
+        {
+            if (num % 2 == 0)
+                return num / 2;
+            else
+                return (num + 1) / 2;
+        }
+    }
+}
